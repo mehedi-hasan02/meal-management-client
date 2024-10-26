acd0b641e0c0713f216ca5b057032d003405efdb
mehedi-hasan02 <mehedisani02@gmail.com>
2024-10-26 11:20:18 +0600

    setup meal-management project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from "./component/shared/navbar/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container mx-auto mt-6 bg-green-500">
+        <Navigation />
+      </div>
+    </>
+  )
+}
+
+export default App
